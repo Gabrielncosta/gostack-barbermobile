7a5c5752710bb43fdc258197f4583448caadfb78
Gabrielncosta <gabrielncosta208@gmail.com>
2020-04-26 20:06:13 -0300

    NestedNavigator

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-/* import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SignIn from './pages/SigIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 const AppStack = createStackNavigator();
-/* const Tab = createBottomTabNavigator(); */
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Routes() {
+  const Signed = useSelector((state) => state.auth.signed);
+
   return (
     <NavigationContainer>
-      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+      <AppStack.Navigator
+        initialRouteName={Signed ? 'Home' : 'SignIn'}
+        screenOptions={{ headerShown: false }}
+      >
         <AppStack.Screen name="SignIn" component={SignIn} />
         <AppStack.Screen name="SignUp" component={SignUp} />
+        <AppStack.Screen name="Home" component={Home} />
       </AppStack.Navigator>
-      {/* <Tab.Navigator>
-        <Tab.Screen name="SignIn" component={SignIn} />
-        <Tab.Screen name="SignUp" component={SignUp} />
-      </Tab.Navigator> */}
     </NavigationContainer>
   );
 }
