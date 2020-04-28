3d55d1233398098dc6ed6fd68f0c4c3765eaa0a0
Gabrielncosta <gabrielncosta208@gmail.com>
2020-04-28 19:52:17 -0300

    Appointment

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,14 +9,40 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SignIn from './pages/SigIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 const AppStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function Home() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        activeTintColor: '#FFF',
+        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+        style: {
+          backgroundColor: '#8d41a8',
+          borderTopColor: '#8d41a8',
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Agendamentos',
+          tabBarIcon: () => <Icon name="event" size={20} color="#fff" />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: () => <Icon name="person" size={20} color="#fff" />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
